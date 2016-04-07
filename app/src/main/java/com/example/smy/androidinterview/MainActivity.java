4edb5747c0a2d12f2cab29dda44a27f1ea3fdc62
NikolayBoyko <bojkoniko25@gmail.com>
2016-04-07 11:47:07 +0300

    add search in Toolbar

--- app/src/main/java/com/example/smy/androidinterview/MainActivity.java
+++ app/src/main/java/com/example/smy/androidinterview/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.smy.androidinterview;
 
 import android.graphics.Color;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +20,13 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setTitle("Android Interview");
         toolbar.setTitleTextColor(TOOLBAR_TEXT_COLOR);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+       // SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.action_search));
+        //searchView.setOnQueryTextListener(this);
+        return super.onCreateOptionsMenu(menu);
     }
 }
