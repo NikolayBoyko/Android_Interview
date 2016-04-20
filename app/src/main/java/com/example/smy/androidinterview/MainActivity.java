37ecd8d855585c6c131bafb08f3c4e95de5702af
skiner <molokoskiner@gmail.com>
2016-04-20 14:38:20 +0300

    Fix  drawer, realization click listener  item drawer

--- app/src/main/java/com/example/smy/androidinterview/MainActivity.java
+++ app/src/main/java/com/example/smy/androidinterview/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.smy.androidinterview;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,23 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setTitle("Android Interview");
         toolbar.setTitleTextColor(Color.BLACK);
         setSupportActionBar(toolbar);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+        );
+
+        mDrawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         super.onContentChanged();
     }
 
@@ -30,4 +54,36 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.toolbar_menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(Gravity.LEFT)) {
+            mDrawerLayout.closeDrawer(Gravity.LEFT);
+        } else super.onBackPressed();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.android:
+                Toast.makeText(this, "Click on Android", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.java:
+                Toast.makeText(this, "Click on Java", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.settings:
+                Toast.makeText(this, "Click on Settings", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.about:
+                Toast.makeText(this, "Click on About", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 }
